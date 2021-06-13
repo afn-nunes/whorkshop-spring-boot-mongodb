b6ccbadf4985b3e3f6f8477e1f20146e3b813dc7
Andre Ferreira Nunes <afn.nunes@gmail.com>
2021-06-13 15:13:20 -0300

    Atualização de usuário com PUT

--- src/main/java/com/andrenunes/workshopmongo/resources/UserResource.java
+++ src/main/java/com/andrenunes/workshopmongo/resources/UserResource.java
@@ -54,4 +54,13 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();	
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO objDTO) {		
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
 }
