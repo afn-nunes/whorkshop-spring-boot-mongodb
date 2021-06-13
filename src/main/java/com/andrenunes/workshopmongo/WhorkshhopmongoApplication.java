493f4bb1bf920730c5a6407d2f79aee68304735b
Andre Ferreira Nunes <afn.nunes@gmail.com>
2021-06-12 23:57:42 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/andrenunes/workshopmongo/WhorkshhopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.andrenunes.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhorkshhopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhorkshhopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrenunes/workshopmongo/WhorkshhopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrenunes.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhorkshhopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
