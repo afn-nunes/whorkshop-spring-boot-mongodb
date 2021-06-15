4e578e1248ac1499d2fc269f2e81fc09c379df0f
Andre Ferreira Nunes <afn.nunes@gmail.com>
2021-06-15 18:32:06 -0300

    Consulta simples com @Query

--- src/main/java/com/andrenunes/workshopmongo/services/PostService.java
+++ src/main/java/com/andrenunes/workshopmongo/services/PostService.java
@@ -22,7 +22,8 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		//return repo.findByTitleContainingIgnoreCase(text); utilizando query methods
+		return repo.searchTitle(text); //Utilizando @query
 	}
 	
 }
